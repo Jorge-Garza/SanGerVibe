45602d35fa36763e16ec25e1e3f4fe013ace8889
Jorge Garza <jorgegarza.its@gmail.com>
2025-02-13 02:02:06 -0600

    Se agrego el proyecto

---
+++ views/templates/header.php
@@ -0,0 +1,19 @@
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace">Registro</a>
+            <a href="/login" class="header__enlace">Iniciar Sesión</a>
+        </nav>
+
+        <div class="header__contenido">
+            <a href="/">
+                <h1 class="header__logo">SanGerVibe</h1>
+            </a>
+
+            <p class="header__texto">Baños, inhalaciones, irrigaciones y calefacción</p>
+            <p class="header__texto header__texto--modalidad">Todo el mes</p>
+
+            <a href="/registro" class="header__boton">Compra tu kit ahora!</a>
+        </div>
+    </div>
+</header>
